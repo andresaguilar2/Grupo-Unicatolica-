a80a73ff8f799df72fd7fd572f6d17b1c5c0a947
Juan felipe Salazar Trujillo <111032429+shikuken@users.noreply.github.com>
2025-03-06 21:46:12 -0500

    agregar menu

--- semana4/Marge sort.py
+++ semana4/Marge sort.py
@@ -43,3 +43,66 @@ def merge_sort(lista, clave):
 def buscar_empleado(criterio, valor):
     resultados = [e for e in empleados if e[criterio] == valor]
     return resultados
+
+#  Menú interactivo
+def menu():
+    while True:
+        print("\n--- BIENVENIDO ---")
+        print("1. Ordenar empleados por edad ")
+        print("2. Ordenar empleados por salario ")
+        print("3. Ordenar empleado por nombre")
+        print("4. Buscar empleado por salario")
+        print("5. Buscar empleado por nombre")
+        print("6. Salir")
+
+        opcion = input("Elige una opción: ")
+
+        if opcion == "1":
+            empleados_ordenados = empleados.copy()
+            merge_sort(empleados_ordenados, "edad")
+            print("\nEmpleados ordenados por edad:")
+            for emp in empleados_ordenados:
+                print(emp)
+        
+        elif opcion == "2":
+            empleados_ordenados = empleados.copy()
+            merge_sort(empleados_ordenados, "salario")
+            print("\nEmpleados ordenados por salario:")
+            for emp in empleados_ordenados:
+                print(emp)
+
+        elif opcion == "3":
+            empleados_ordenados = empleados.copy()
+            merge_sort(empleados_ordenados, "nombre")
+            print("\nEmpleados ordenados por nombre:")
+            for emp in empleados_ordenados:
+                print(emp)
+
+        elif opcion == "4":
+            salario = int(input("Ingrese el salario a buscar: "))
+            resultados = buscar_empleado("salario", salario)
+            if resultados:
+                print("\nEmpleados con salario", salario, ":")
+                for emp in resultados:
+                    print(emp)
+            else:
+                print("No se encontraron empleados con ese salario.")
+
+        elif opcion == "5":
+            edad = int(input("Ingrese la edad a buscar: "))
+            resultados = buscar_empleado("edad", edad)
+            if resultados:
+                print("\nEmpleados con edad", edad, ":")
+                for emp in resultados:
+                    print(emp)
+            else:
+                print("No se encontraron empleados con esa edad.")
+
+        elif opcion == "6":
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opción inválida. Intente de nuevo.")
+
+# Ejecutar el menú interactivo
+menu()
